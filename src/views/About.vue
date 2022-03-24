de98c97e718e17b16843a7e65a32b4bef9743bde
Catalina Tapia <catalinantf@gmail.com>
2022-03-24 17:00:44 -0300

    sigunetecommit

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,35 +10,49 @@
       bg-variant="transparent"
       border-variant="light"
     >
-        <b-container class="py-4" >
-      <b-card-text>
-        <p>
-          Mi nombre es Catalina Tapia, soy Desarrolladora Frontend en constante
-          aprendizaje, recién egresada de la décima generación del Bootcamp en
-          Desafío Latam. Las skills que he adquirido son HTML, CSS, JavaScript,l
-          framework VueJs y diseño responsivo, además ocupo la librería
-          Bootstrap, y el preprocesador de CSS Sass; por último conozco algunas
-          herramientas de testing y del sistema de control de versiones Git.
-        </p>
-        <p>
-          Mis metas son seguir aprendiendo, practicando y poder tener
-          experiencia trabajando en equipo.
-        </p>
+      <b-container class="py-4">
+        <b-card-text>
+          <p>
+            Mi nombre es Catalina Tapia, soy Desarrolladora Frontend en
+            constante aprendizaje, recién egresada de la décima generación del
+            Bootcamp en Desafío Latam. Las skills que he adquirido son HTML,
+            CSS, JavaScript,l framework VueJs y diseño responsivo, además ocupo
+            la librería Bootstrap, y el preprocesador de CSS Sass; por último
+            conozco algunas herramientas de testing y del sistema de control de
+            versiones Git.
+          </p>
+          <p>
+            Mis metas son seguir aprendiendo, practicando y poder tener
+            experiencia trabajando en equipo.
+          </p>
 
-        <small>
-          Puedes ver más de mi trabajo 
-          <a href="https://github.com/Catalinantf " target="_blank">acá</a>
-          
-          <br>
-          Mi proyecto final de carrera 
-          <a href="https://academia-ventas.web.app/home" target="_blank">aquí</a>
-                    <br>
-          Mi CV 
-          <a href="https://drive.google.com/file/d/1GjbLAG7Xd_-x6CowKAlENwBcaaryEgpD/view?usp=sharing" target="_blank">aquí</a>
-          
-        </small>
-      </b-card-text>
-        </b-container>
+          <small>
+            Puedes ver más de mi trabajo
+            <strong>
+              <a href="https://github.com/Catalinantf " target="_blank"
+                >acá</a
+              ></strong
+            >
+
+            <br />
+            Mi proyecto final de carrera
+            <strong>
+              <a href="https://academia-ventas.web.app/home" target="_blank"
+                >aquí</a
+              ></strong
+            >
+            <br />
+            Mi CV
+            <strong>
+              <a
+                href="https://drive.google.com/file/d/1GjbLAG7Xd_-x6CowKAlENwBcaaryEgpD/view?usp=sharing"
+                target="_blank"
+                >aquí</a
+              ></strong
+            >
+          </small>
+        </b-card-text>
+      </b-container>
     </b-card>
   </div>
 </template>
